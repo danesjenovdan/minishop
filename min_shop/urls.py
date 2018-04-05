5249f7ad55af72e7c171ba9ce59bf6ed4d6d94a6
Muki <filipdobranic@gmail.com>
2018-04-05 12:04:25 +0000

    add empty root url

--- min_shop/urls.py
+++ min_shop/urls.py
@@ -17,9 +17,10 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-
+from shop.views import index
 
 urlpatterns = [
+    url(r'^$', index),
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include('shop.urls_api')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
