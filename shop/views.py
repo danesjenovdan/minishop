e06a1dd77bb6c77d166f15f49a9158e119b26750
Tomaž <cofek0@gmail.com>
2018-04-09 10:46:59 +0200

    add UPN generator

--- shop/views.py
+++ shop/views.py
@@ -1,21 +1,26 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from django.views.decorators.csrf import csrf_exempt
+
+import json
 
 def index(requst):
     return HttpResponse("")
 
+
+@csrf_exempt
 def poloznica(request):
-    
+    data = json.loads(request.body.decode('utf8'))
     bill = {}
-    bill['id'] = request.GET.get('id')
-    bill['date'] = request.GET.get('date')
-    bill['price'] = request.GET.get('price')
-    bill['referencemath'] = 98 - int(bill['id']) % 97
-    bill['code'] = request.GET.get('code')
+    bill['id'] = data.get('id')
+    bill['date'] = data.get('date')
+    bill['price'] = data.get('price')
+    bill['referencemath'] = data.get('reference')
+    bill['code'] = data.get('code')
     
     victim = {}
-    victim['name'] = request.GET.get('name')
-    victim['address1'] = request.GET.get('address1')
-    victim['address2'] = request.GET.get('address2')
+    victim['name'] = data.get('name')
+    victim['address1'] = data.get('address1')
+    victim['address2'] = data.get('address2')
     
     return render_to_response('poloznica.html', {'victim': victim, 'bill': bill})
