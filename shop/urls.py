4974be59884b80cbde0b32236b059c4f426f4aaf
Muki <filipdobranic@gmail.com>
2018-04-09 14:31:45 +0000

    add char to url

--- shop/urls.py
+++ shop/urls.py
@@ -3,4 +3,4 @@ from . import views
 
 urlpatterns = [
     url(r'^poloznica/', views.poloznica),
-    url(r'^upn_pdf/(?P<pk>[ÖÜØÄÂÁÉÓÚÍÎöüøäâáéóúíîčćšžČĆŠŽa-zA-Z0-9 \-\+!"%\.,:]+)', views.getPDFodOrder)]
+    url(r'^upn_pdf/(?P<pk>[ÖÜØÄÂÁÉÓÚÍÎöüøäâáéóúíîčćšžČĆŠŽa-zA-Z0-9 \-\+!"%\.,:\_]+)', views.getPDFodOrder)]
